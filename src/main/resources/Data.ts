2951788886e3168736e37d695f6d73fe4543ef56
ericrkuo <ericrkuo@gmail.com>
2020-12-06 19:43:51 -0800

    updated smaple data

--- src/main/resources/Data.ts
+++ src/main/resources/Data.ts
@@ -11,32 +11,32 @@ const user3 = new User("andrewzulaybar@gmail.com", "Andrew Zulaybar", "https://a
 const user4 = new User("superuser@gmail.com", "superuser", "", 0, Role.SUPER_USER);
 
 // todo: make these by calling spoonacular API on the dishes provided
-const recipe1 = new Recipe("123", "test", "test");
-const recipe2 = new Recipe("123", "test", "test");
-const recipe3 = new Recipe("123", "test", "test");
-const recipe4 = new Recipe("123", "test", "test");
-const recipe5 = new Recipe("123", "test", "test");
-const recipe6 = new Recipe("123", "test", "test");
-const recipe7 = new Recipe("123", "test", "test");
-const recipe8 = new Recipe("123", "test", "test");
-const recipe9 = new Recipe("123", "test", "test");
-const recipe10 = new Recipe("123", "test", "test");
-const recipe11 = new Recipe("123", "test", "test");
-const recipe12 = new Recipe("123", "test", "test");
+const recipe1 = new Recipe("123", "Chirashi Sushi Cake and Temari Sushi", "http://www.justonecookbook.com/recipes/chirashi-sushi-cake-and-temari-sushi-and-giveaway-winners/");
+const recipe2 = new Recipe("123", "Deconstructed Sushi (Chirashi Sushi)", "http://www.thevintagemixer.com/2012/04/deconstructed-sushi-bowl-recipe/");
+const recipe3 = new Recipe("123", "Make your own Korean BBQ skewers", "http://www.justonecookbook.com/recipes/chirashi-sushi-cake-and-temari-sushi-and-giveaway-winners/");
+const recipe4 = new Recipe("123", "Korean BBQ", "http://www.justonecookbook.com/recipes/chirashi-sushi-cake-and-temari-sushi-and-giveaway-winners/");
+const recipe5 = new Recipe("123", "Grandma's Schnitzels", "http://www.justonecookbook.com/recipes/chirashi-sushi-cake-and-temari-sushi-and-giveaway-winners/");
+const recipe6 = new Recipe("123", "German Authnetic Schnitzels", "http://www.justonecookbook.com/recipes/chirashi-sushi-cake-and-temari-sushi-and-giveaway-winners/");
+const recipe7 = new Recipe("123", "Amygdalota at Home", "http://www.justonecookbook.com/recipes/chirashi-sushi-cake-and-temari-sushi-and-giveaway-winners/");
+const recipe8 = new Recipe("123", "Best Amygdalota across Greek", "http://www.justonecookbook.com/recipes/chirashi-sushi-cake-and-temari-sushi-and-giveaway-winners/");
+const recipe9 = new Recipe("123", "PiriPiri home made style", "http://www.justonecookbook.com/recipes/chirashi-sushi-cake-and-temari-sushi-and-giveaway-winners/");
+const recipe10 = new Recipe("123", "Best PiriPiri", "http://www.justonecookbook.com/recipes/chirashi-sushi-cake-and-temari-sushi-and-giveaway-winners/");
+const recipe11 = new Recipe("123", "NY Style - Chef Augistine's Take", "http://www.justonecookbook.com/recipes/chirashi-sushi-cake-and-temari-sushi-and-giveaway-winners/");
+const recipe12 = new Recipe("123", "How to make the best cheese pizza", "http://www.justonecookbook.com/recipes/chirashi-sushi-cake-and-temari-sushi-and-giveaway-winners/");
 
 // todo: make these by calling spoonacular API on the dishes provided
-const nutrition1 = new Nutrition("", 0, "");
-const nutrition2 = new Nutrition("", 0, "");
-const nutrition3 = new Nutrition("", 0, "");
-const nutrition4 = new Nutrition("", 0, "");
-const nutrition5 = new Nutrition("", 0, "");
-const nutrition6 = new Nutrition("", 0, "");
-const nutrition7 = new Nutrition("", 0, "");
-const nutrition8 = new Nutrition("", 0, "");
-const nutrition9 = new Nutrition("", 0, "");
-const nutrition10 = new Nutrition("", 0, "");
-const nutrition11 = new Nutrition("", 0, "");
-const nutrition12 = new Nutrition("", 0, "");
+const nutrition1 = new Nutrition("Calories", 300, "calories");
+const nutrition2 = new Nutrition("Fat", 5, "%");
+const nutrition3 = new Nutrition("Sugar", 25, "g");
+const nutrition4 = new Nutrition("Carbs", 100, "g");
+const nutrition5 = new Nutrition("Protein", 100, "g");
+const nutrition6 = new Nutrition("Sugar", 10, "g");
+const nutrition7 = new Nutrition("Calories", 500, "calories");
+const nutrition8 = new Nutrition("Carbs", 100, "g");
+const nutrition9 = new Nutrition("Protein", 10, "g");
+const nutrition10 = new Nutrition("Salt", 100, "mg");
+const nutrition11 = new Nutrition("Saturated Fat", 15, "%");
+const nutrition12 = new Nutrition("Protein", 20, "g");
 
 
 const dish1 = new Dish(user1, [recipe1, recipe2], [nutrition1, nutrition2], "Giant Sushi", "I love sushi and this was my first time making it", CuisineType.JAPANESE, ["rice", "salmon", "cucumber", "seaweed"], "test", "test", "https://user-images.githubusercontent.com/49849754/101288415-4a467980-37ab-11eb-8a3a-ae40e73d2e6d.png");
